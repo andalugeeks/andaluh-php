dde2ce5d4c5e60bd6a4227dfa07f294928179c41
Leon Alvarez Del Canto <leon@leon-design.es>
2020-09-20 01:58:17 +0200

    BaseRule Refactor

--- lib/Andaluh/Rules/BaseRule.php
+++ lib/Andaluh/Rules/BaseRule.php
@@ -7,16 +7,31 @@ abstract class BaseRule
 {
     public function keepCase(string $word, string $replacementWord): string
     {
-        if (mb_convert_case($word, MB_CASE_LOWER, "UTF-8") === $word) {
+        if (self::isLowerCase($word)) {
             return mb_convert_case($replacementWord, MB_CASE_LOWER, "UTF-8");
         }
-        if (mb_convert_case($word, MB_CASE_UPPER, "UTF-8") === $word) {
+        if (self::isUpperCase($word)) {
             return mb_convert_case($replacementWord, MB_CASE_UPPER, "UTF-8");
         }
-        if (mb_convert_case($word, MB_CASE_TITLE, "UTF-8") === $word) {
+        if (self::isTitleCase($word)) {
             return mb_convert_case($replacementWord, MB_CASE_TITLE, "UTF-8");
         }
 
         return $replacementWord;
+    }
+
+    static function isLowerCase(string $word): bool
+    {
+        return mb_convert_case($word, MB_CASE_LOWER, "UTF-8") === $word;
+    }
+
+    static function isUpperCase(string $word): bool
+    {
+        return mb_convert_case($word, MB_CASE_UPPER, "UTF-8") === $word;
+    }
+
+    static function isTitleCase(string $word): bool
+    {
+        return mb_convert_case($word, MB_CASE_TITLE, "UTF-8") === $word;
     }
 }
