ec99430d2f7b92441299cb7ad320ff689f88168d
Leon Alvarez Del Canto <leon@leon-design.es>
2020-09-20 12:35:42 +0200

    clean BaseRuleTest

--- tests/Rules/BaseRuleTest.php
+++ tests/Rules/BaseRuleTest.php
@@ -11,30 +11,27 @@ class BaseRuleTest extends TestCase
     /** @test */
     public function it_keep_lower_case()
     {
-        $rule = $this->getMockForAbstractClass(BaseRule::class);
         $this->assertEquals(
             "replace converted to lower",
-            $rule::keepCase("this is lower case", "REPLACE CONVERTED TO LOWER")
+            BaseRule::keepCase("this is lower case", "REPLACE CONVERTED TO LOWER")
         );
     }
 
     /** @test */
     public function it_keep_upper_case()
     {
-        $rule = $this->getMockForAbstractClass(BaseRule::class);
         $this->assertEquals(
             "REPLACE CONVERTED TO UPPER",
-            $rule::keepCase("THIS IS UPPER CASE", "replace converted to upper")
+            BaseRule::keepCase("THIS IS UPPER CASE", "replace converted to upper")
         );
     }
 
     /** @test */
     public function it_keep_title_case()
     {
-        $rule = $this->getMockForAbstractClass(BaseRule::class);
         $this->assertEquals(
             "Replace Converted To Title",
-            $rule::keepCase("This Is Title Case", "replace converted to title")
+            BaseRule::keepCase("This Is Title Case", "replace converted to title")
         );
     }
 }
