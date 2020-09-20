e34746a75957989fe57fcba2f6769e6fe0c719f8
Leon Alvarez Del Canto <leon@leon-design.es>
2020-09-20 16:40:47 +0200

    VAFRules vaf parameter

--- lib/Andaluh/Rules/VAFRules.php
+++ lib/Andaluh/Rules/VAFRules.php
@@ -4,30 +4,34 @@ namespace Andaluh\Rules;
 
 class VAFRules extends BaseRule
 {
-    public static function apply(string $text): string
+    public static function apply(string $text, string $vaf = self::VAF): string
     {
         return preg_replace_callback_array(
             [
                 // servilleta => servilleta
                 // zarzamora => zarzamora
                 // gasoducto => gaçoducto
-                '/(z|s)(a|e|i|o|u|á|é|í|ó|ú|â|ê|î|ô|û)/i' => [self::class, 'replaceVAF'],
+                '/(z|s)(a|e|i|o|u|á|é|í|ó|ú|â|ê|î|ô|û)/i' => function ($match) use ($vaf) {
+                    return self::replaceVAF($match, $vaf);
+                },
                 // cecina => çeçina
-                '/(c)(e|i|é|í|ê|î)/i' => [self::class, 'replaceVAF'],
+                '/(c)(e|i|é|í|ê|î)/i' => function ($match) use ($vaf) {
+                    return self::replaceVAF($match, $vaf);
+                },
             ],
             $text
         );
     }
 
-    public static function replaceVAF(array $match): string
+    public static function replaceVAF(array $match, string $vaf = self::VAF): string
     {
         $lChar = $match[1];
         $nextChar = $match[2];
 
         if (self::isLowerCase($lChar)) {
-            return self::VAF . $nextChar;
+            return "{$vaf}{$nextChar}";
         }
 
-        return self::toUpperCase(self::VAF) . $nextChar;
+        return self::toUpperCase($vaf) . $nextChar;
     }
 }
