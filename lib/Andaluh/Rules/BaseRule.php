001b2ccd263751489fc6e8ce6c0ea15faf755522
Leon Alvarez Del Canto <leon@leon-design.es>
2020-09-20 02:44:11 +0200

    BaseRule add case converters

--- lib/Andaluh/Rules/BaseRule.php
+++ lib/Andaluh/Rules/BaseRule.php
@@ -20,18 +20,33 @@ abstract class BaseRule
         return $replacementWord;
     }
 
+    static function toLowerCase(string $word): string
+    {
+        return mb_convert_case($word, MB_CASE_LOWER, "UTF-8");
+    }
+
     static function isLowerCase(string $word): bool
     {
-        return mb_convert_case($word, MB_CASE_LOWER, "UTF-8") === $word;
+        return self::toLowerCase($word) === $word;
+    }
+
+    static function toUpperCase(string $word): string
+    {
+        return mb_convert_case($word, MB_CASE_UPPER, "UTF-8");
     }
 
     static function isUpperCase(string $word): bool
     {
-        return mb_convert_case($word, MB_CASE_UPPER, "UTF-8") === $word;
+        return self::toUpperCase($word) === $word;
+    }
+
+    static function toTitleCase(string $word): string
+    {
+        return mb_convert_case($word, MB_CASE_TITLE, "UTF-8");
     }
 
     static function isTitleCase(string $word): bool
     {
-        return mb_convert_case($word, MB_CASE_TITLE, "UTF-8") === $word;
+        return self::toTitleCase($word) === $word;
     }
 }
