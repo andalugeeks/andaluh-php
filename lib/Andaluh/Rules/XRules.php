dc13834b97a1ab4a7ad8568758069915fee1f843
Leon Alvarez Del Canto <leon@leon-design.es>
2020-09-20 16:43:15 +0200

    XRules pass vaf

--- lib/Andaluh/Rules/XRules.php
+++ lib/Andaluh/Rules/XRules.php
@@ -19,17 +19,17 @@ class XRules extends BaseRule
             [
                 # If the /ks/ sound is between vowels
                 # Axila => Aççila | Éxito => Éççito | Sexy => Çeççy
-                '/(a|e|i|o|u|á|é|í|ó|ú)(x)(a|e|i|o|u|y|á|é|í|ó|ú)/iu' => function ($match) {
+                '/(a|e|i|o|u|á|é|í|ó|ú)(x)(a|e|i|o|u|y|á|é|í|ó|ú)/iu' => function ($match) use ($vaf) {
                     $prev_char = $match[1];
                     $x_char = $match[2];
                     $next_char = $match[3];
                     $prev_char = self::getVowelCircumflexs($prev_char);
 
                     if (self::isUpperCase($x_char)) {
-                        $upperVaf = self::toUpperCase(self::VAF);
+                        $upperVaf = self::toUpperCase($vaf);
                         return "{$prev_char}{$upperVaf}{$upperVaf}{$next_char}";
                     }
-                    return $prev_char . str_repeat(self::VAF, 2) . $next_char;
+                    return $prev_char . str_repeat($vaf, 2) . $next_char;
                 },
             ],
             $text
