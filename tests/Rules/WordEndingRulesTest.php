0f34977852b7d4af955299fc3cf705c950382c87
Leon Alvarez Del Canto <leon@leon-design.es>
2020-09-20 19:21:40 +0200

    WordEndingRules Test

--- tests/Rules/WordEndingRulesTest.php
+++ tests/Rules/WordEndingRulesTest.php
@@ -7,5 +7,17 @@ use PHPUnit\Framework\TestCase;
 
 class WordEndingRulesTest extends TestCase
 {
-    //TODO TESTCASES
+    /** @test */
+    public function it_pass_ending_exceptions()
+    {
+        $this->assertEquals(
+            'fado despido cado bûççido',
+            WordEndingRules::apply('fado despido cado bûççido')
+        );
+
+        $this->assertEquals(
+            'áê sonaO sonâh sáê saeps enFadá enfadáô espío despdo',
+            WordEndingRules::apply('áeps sonadO sonaDas sáeps saeps enFadADa enfadaDos espío despdo')
+        );
+    }
 }
