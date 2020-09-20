f7f2fd156d10023b28b3df1b881fa4b70f3d31f4
Leon Alvarez Del Canto <leon@leon-design.es>
2020-09-20 16:21:15 +0200

    WordInteractionRules

---
+++ tests/Rules/WordInteractionRulesTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+use Andaluh\Rules\WordInteractionRules;
+use PHPUnit\Framework\TestCase;
+
+class WordInteractionRulesTest extends TestCase
+{
+    // TODO TESTS CASE
+}
